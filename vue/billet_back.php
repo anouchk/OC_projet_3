1f49cc2762d87c92909feee3863d2aca37e63208
anouchk <analutzky@gmail.com>
2018-02-08 10:59:32 +0100

    confection de billet_back.php

--- vue/billet_back.php
+++ vue/billet_back.php
@@ -6,6 +6,13 @@
         <title>Billet simple pour l'Alaska </title>
 
     	<link href="vue/style.css" rel="stylesheet" /> 
+    	<script type="text/javascript" src="XXXXX/tiny_mce/tiny_mce.js"></script>
+    	<script type="text/javascript">
+	        tinyMCE.init({
+	            mode : "textareas",
+	            theme : "simple"
+	        });
+     	</script>
 
     </head>
 
@@ -13,10 +20,10 @@
 
         <a href="blog.php?section=index">Retour à la liste des billets</a>
 
-			<form method="post" action="cible.php">
-				<p><label> Titre</label> : <input type="text" name="pseudo"></p>
-				<p><label> Contenu de l'épisode</label> : <input type="text" name="message"></p>
-				<input type ="hidden" name="id2_billet" value="<?php echo $_GET['billet']?>">
+			<form name="" id="" method="post" action="billet_back.php">
+				<p><label> Titre</label> : <input type="text" name="titre"></p>
+				<p><textarea> Contenu de l'épisode</textarea> : <input type="text" name="contenu"></p>
+				<input type ="hidden" name="" value="">
 				<p><input type="submit" ></p>
 			</form>
 
