d77dd4c7234d62cd53d2adb03716d1b9fc805fc9
anouchk <analutzky@gmail.com>
2018-02-14 17:27:52 +0100

    login en MVC suite

--- controleur/login.php
+++ controleur/login.php
@@ -1,2 +1,20 @@
 <?php
-include_once('vue/login.php'); 
+if (!isset($_POST['pseudo']) && !isset($_POST['pass'])) {
+	include_once('vue/login.php'); 
+} else {
+	include_once('modele/login.php');
+		// etape 2 : comparer le pass du hash avec celui entré par le formulaire de connexion
+	if (password_verify($_POST['pass'], $resultat['pass'])) {
+	    session_start();
+	    $_SESSION['id'] = $resultat['id'];
+	    $_SESSION['pseudo'] = $pseudo;
+	    echo 'Vous êtes connecté !';
+	} else {
+	    echo 'Mauvais identifiant ou mot de passe !';
+	}
+
+	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+	{
+	    echo '<br><br>Bonjour ' . $_SESSION['pseudo'];
+	}
+}
--- modele/login.php
+++ modele/login.php
@@ -3,30 +3,10 @@
 // Préparer la requête (:pseudo ça correspondra à $pseudo)
 $req = $bdd->prepare('SELECT id, pass, pseudo FROM auteur WHERE pseudo = :pseudo');
 // Initialisation de la variable récupérant le pseudo entré par l'utilisateur à la connexion
-if (isset($_POST['pseudo'])) {
-	$pseudo = $_POST['pseudo'];
-};
+$pseudo = $_POST['pseudo'];
 
-if (isset($_POST['pass'])) {
-	$pass = $_POST['pass'];
-};
 // Exécuter la requête : ça exécute et ça nous retourne le nombre de lignes affectées
 $req->execute(array('pseudo' => $pseudo));
 // Fetch permet de récupérer le résultat de la requête et de le renvoyer sous une certaine forme (tableau par exemple)
 $resultat = $req->fetch(PDO::FETCH_ASSOC);
 // var_dump($resultat); die;
-
-// etape 2 : comparer le pass du hash avec celui entré par le formulaire de connexion
-if (password_verify($_POST['pass'], $resultat['pass'])) {
-    session_start();
-    $_SESSION['id'] = $resultat['id'];
-    $_SESSION['pseudo'] = $pseudo;
-    echo 'Vous êtes connecté !';
-} else {
-    echo 'Mauvais identifiant ou mot de passe !';
-}
-
-if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-{
-    echo '<br><br>Bonjour ' . $_SESSION['pseudo'];
-}
