a09249ccd18bb077e5eac52bc170ddc815b8ac2f
anouchk <analutzky@gmail.com>
2018-02-15 11:57:59 +0100

    exécuter la fonction get_billets() dans billets_back.php

--- controleur/billets_back.php
+++ controleur/billets_back.php
@@ -1,6 +1,9 @@
 <?php
 include_once('modele/get_billets.php'); 
 
+// Exécuter la fonction get_billets(), avec les OFFSET et les LIMIT obligatoires en paramètres (à enlever d'ailleurs ?)
+$billets = get_billets(0,5);
+
 // On effectue du traitement sur les données (contrôleur) 
 // Ici, on doit surtout sécuriser l'affichage 
 foreach($billets as $cle => $billet) 
