7c47b8f8c604f4f2f389dce0bb089a083f8b9bec
anouchk <analutzky@gmail.com>
2018-02-15 17:18:08 +0100

    construction de la fonction update_billet()

--- modele/post_billet.php
+++ modele/post_billet.php
@@ -1,9 +1,9 @@
 <?php
-function post_commentaire () {
+function post_billet () {
 	// Récupération de la base de données
     global $bdd;
 
-	// Effectuer ici la requête qui insère le message reçu avec $_POST dans la base de données 
+	// Effectuer ici la requête qui insère le billet rédigé avec $_POST dans la base de données 
 	$requete = $bdd->prepare('INSERT INTO billets(id,titre,contenu, date_creation) VALUES(:id, :titre, :contenu, :date_creation)'); 
 	$date = date('Y-m-d H:i:s');
 	$requete->bindParam(':id', $_POST['id']);
@@ -11,4 +11,19 @@ function post_commentaire () {
 	$requete->bindParam(':contenu', $_POST['contenu']);
 	$requete->bindParam(':date_creation', $date);
 	$requete->execute();
+}
+
+function update_billet () {
+	// Récupération de la base de données
+    global $bdd;
+
+    // Effectuer ici la requête qui met à jour le billet avec $_POST dans la base de données 
+    $requete = $bdd->prepare('UPDATE billets
+    SET column1 = value1, column2 = value2, ...
+    WHERE condition'); 
+}
+
+function delete_billet () {
+
+
 }
