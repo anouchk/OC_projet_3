5a77a0d254ef6d6b622d321443a6e67e47e7b522
anouchk <analutzky@gmail.com>
2018-02-21 21:17:12 +0100

    ajout de la fonction delete_commentaire() dans le modele

--- modele/delete_commentaire.php
+++ modele/delete_commentaire.php
@@ -1,9 +1,13 @@
 <?php
-if (isset($_POST['idCommentaire'])) {
- $id = $_POST['idCommentaire'];
- $sql = "DELETE FROM commentaires WHERE id = :id";
- $q = array('id' => $id);
- $req = $bdd -> prepare($sql);
- $req -> execute($q);
- header('Location:controleur/commentaires_back.php');
+
+delete_commentaire() {
+	if (isset($_POST['idCommentaire'])) {
+	 $id = $_POST['idCommentaire'];
+	 $sql = "DELETE FROM commentaires WHERE id = :id";
+	 $q = array('id' => $id);
+	 $req = $bdd -> prepare($sql);
+	 $req -> execute($q);
+	 header('Location:controleur/commentaires_back.php');
+	}
 }
+	
