ab3b056463b099b81c1cdb2c45ced229041a50c8
anouchk <analutzky@gmail.com>
2018-02-23 20:56:55 +0100

    stocker l'affichage sécurisé et le lancement de la fonction delete_commentaire() dans une fonction

--- controleur/commentaires_back.php
+++ controleur/commentaires_back.php
@@ -20,27 +20,27 @@ function commentaires_back_suppression_commentaire()
     var_dump($idBillet);
     $commentaires = affichage_securise($idBillet);
 
-    // On redirige
+    // On redirige 
     header('Location: Blog.php?section=commentaires_back&billet='.$idBillet);
 }
 
+function affichage_securise($idBillet) {
+	// Exécuter la fonction get_billets(), avec les OFFSET et les LIMIT obligatoires en paramètres (à enlever d'ailleurs ?)
+	$commentaires = get_commentaires(0, 15, $idBillet);
 
-// Exécuter la fonction get_billets(), avec les OFFSET et les LIMIT obligatoires en paramètres (à enlever d'ailleurs ?)
-$commentaires = get_commentaires(0, 15, $idBillet);
+	// On effectue du traitement sur les données (contrôleur) 
+	// Ici, on doit surtout sécuriser l'affichage 
+	foreach($commentaires as $cle => $commentaire) 
+	{ 
+	    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
+	    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
+	} 
+	 
+	if (!empty($_POST['idCommentaire'])) {
+		// delete_commentaire();
+		echo "l'id du commentaire est stocké et la function delete_commentaire est intégrée";
+		var_dump($_POST['idCommentaire']);
+	}
 
-// On effectue du traitement sur les données (contrôleur) 
-// Ici, on doit surtout sécuriser l'affichage 
-foreach($commentaires as $cle => $commentaire) 
-{ 
-    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
-    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
-} 
- 
-if (!empty($_POST['idCommentaire'])) {
-	// delete_commentaire();
-	echo "l'id du commentaire est stocké et la function delete_commentaire est intégrée";
-	var_dump($_POST['idCommentaire']);
+	return $commentaires;
 }
-
-// On affiche la page (vue) 
-include_once('vue/commentaires_back.php'); 
