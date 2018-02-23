3ba582ab3de32c5e0e80e6d54f3e9396128c8d76
anouchk <analutzky@gmail.com>
2018-02-23 21:03:31 +0100

    perfectionnement de delete_commentaire() : paramètres d'id, redirection et variable globale

--- modele/delete_commentaire.php
+++ modele/delete_commentaire.php
@@ -1,13 +1,15 @@
 <?php
 
-function delete_commentaire() {
+function delete_commentaire($id, $id_billet) {
+	global $bdd;
+
 	if (isset($_POST['idCommentaire'])) {
 	 $id = $_POST['idCommentaire'];
 	 $sql = "DELETE FROM commentaires WHERE id = :id";
 	 $q = array('id' => $id);
 	 $req = $bdd -> prepare($sql);
 	 $req -> execute($q);
-	 header('Location:controleur/commentaires_back.php');
+	 header('Location:controleur/commentaires_back.php&billet='.$id_billet);
 	}
 }
 	
