20c31a2a19bdf9f5df3cf5007bb80cad3773bac6
anouchk <analutzky@gmail.com>
2018-02-23 21:16:53 +0100

    est plus précis que

--- modele/delete_commentaire.php
+++ modele/delete_commentaire.php
@@ -1,12 +1,12 @@
 <?php
 
-function delete_commentaire($id, $id_billet) {
+function delete_commentaire($id_commentaire, $id_billet) {
 	global $bdd;
 
 	if (isset($_POST['idCommentaire'])) {
-	 $id = $_POST['idCommentaire'];
-	 $sql = "DELETE FROM commentaires WHERE id = :id";
-	 $q = array('id' => $id);
+	 $id_commentaire = $_POST['idCommentaire'];
+	 $sql = "DELETE FROM commentaires WHERE id = :id_commentaire";
+	 $q = array('id' => $id_commentaire);
 	 $req = $bdd -> prepare($sql);
 	 $req -> execute($q);
 	 header('Location:controleur/commentaires_back.php&billet='.$id_billet);
