67c6250d53c2606c8d7fd445afe80a3646362253
anouchk <analutzky@gmail.com>
2018-03-02 11:22:15 +0100

    appliquer le ORDER BY aux dates au format anglais et non français

--- modele/get_commentaires.php
+++ modele/get_commentaires.php
@@ -16,7 +16,7 @@ function get_commentaires($offset, $limit, $idBillet) {
     $limit = (int) $limit;
 
 	// Récupération des commentaires
-	$PDO_statement = $bdd->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr, signalement FROM commentaires WHERE id_billet=:id ORDER BY date_commentaire_fr DESC LIMIT :offset, :limit');
+	$PDO_statement = $bdd->prepare('SELECT id, auteur, commentaire, DATE_FORMAT(date_commentaire, \'%d/%m/%Y à %Hh%imin%ss\') AS date_commentaire_fr, signalement FROM commentaires WHERE id_billet=:id ORDER BY date_commentaire DESC LIMIT :offset, :limit');
     $PDO_statement->bindParam(':offset', $offset, PDO::PARAM_INT);
     $PDO_statement->bindParam(':limit', $limit, PDO::PARAM_INT);
     $PDO_statement->bindParam(':id', $idBillet, PDO::PARAM_INT);
