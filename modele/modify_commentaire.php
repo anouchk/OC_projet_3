08a1cacff7cbdb2b7d6d2d1af763648a1403f333
anouchk <analutzky@gmail.com>
2018-03-05 23:49:03 +0100

    tentative de requête UPDATE

--- modele/modify_commentaire.php
+++ modele/modify_commentaire.php
@@ -4,19 +4,13 @@ function update_commentaire () {
 	// Récupération de la base de données
     global $bdd;
 
-    // Effectuer ici la requête qui met à jour le commentaire avec $_POST dans la base de données 
-	//     $requete = $bdd->prepare('UPDATE commentaires
-	//     SET column1 = value1, column2 = value2, ...
-	//     WHERE condition'); 
-	// }
-
-    // INSPIRATION : la requeête pour créer un commentaire
-	// Effectuer ici la requête qui insère le message reçu avec $_POST dans la base de données 
-	$requete = $bdd->prepare('INSERT INTO commentaires(id_billet,auteur,commentaire, date_commentaire) VALUES(:id_billet, :auteur, :commentaire, :date_commentaire)'); 
-	$date = date('Y-m-d H:i:s');
-	$requete->bindParam(':id_billet', $_POST['id2_billet']);
+    //Effectuer ici la requête qui met à jour le commentaire avec $_POST dans la base de données 
+	$requete = $bdd->prepare('
+		UPDATE commentaires
+		SET auteur = ':auteur', commentaire = ':commentaire'
+		WHERE id=?');
+	')
 	$requete->bindParam(':auteur', $_POST['pseudo']);
 	$requete->bindParam(':commentaire', $_POST['message']);
-	$requete->bindParam(':date_commentaire', $date);
-	$requete->execute();
+	$req->execute(array($_POST['idCommentaireModified']));
 }
