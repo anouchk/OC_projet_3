3a4542fc63eee0c1f4a2bdf7a671b7c552f956c8
anouchk <analutzky@gmail.com>
2018-03-05 23:21:49 +0100

    mise en forme

--- vue/commentaire_back.php
+++ vue/commentaire_back.php
@@ -1,11 +1,14 @@
-<?php echo "ceci est la vue de commentaire_back"; ?>
 <!DOCTYPE html>
 
 <html>
     <head>
         <meta charset="utf-8" />
         <title>Billet simple pour l'Alaska</title>
-        <link type="text/css" href="vue/style.css" rel="stylesheet" /> 
+        <link type="text/css" href="assets/css/style.css" rel="stylesheet" /> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     </head>
 
     <body>
@@ -15,13 +18,14 @@
     	<!-- il va falloir voir comment récupérer l'id du billet -->
     	<!-- Pour modifier : je veux modifier le contenu du le commentaire dont l'id sera récupéré en POST-->
     	<form action="blog.php?section=enregistrer_modification_commentaire&billet=<?php echo $billet['id']; ?>" method="post">
-				<p><label> Pseudo</label> : <input type="text" name="pseudo" value= "<?php echo $commentaire['auteur'] ?>"</p>
-				<p>Le <?php echo $commentaire['date_commentaire_fr'] ; ?></p>
-				<p><label> Message</label> : <input type="text" name="message" value= "<?php echo $commentaire['commentaire'] ?>"</p>
+                <p>Posté le <?php echo $commentaire['date_commentaire_fr'] ; ?> par <?php echo $commentaire['auteur'] ?></p>
+			
+				<p><label> Message</label> : <input class="input_large" type="text" name="message" value= "<?php echo $commentaire['commentaire'] ?>"</p>
 				<input type ="hidden" name="id2_billet" value="<?php echo $_GET['billet']?>">
 				<p><input type="submit" value="Enregistrer la modification du commentaire" ></p>
 		</form>	
 
     </body>
+
 </html>
 
