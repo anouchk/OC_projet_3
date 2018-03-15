434c59a7c752495b4eda40425b317bb6a1d49e78
anouchk <analutzky@gmail.com>
2018-03-15 14:18:52 +0100

    tunage de controleur/commentaires

--- controleur/commentaires.php
+++ controleur/commentaires.php
@@ -1,41 +1,30 @@
 <?php 
-// On demande les 10 derniers commentaires (modèle) 
 include_once('modele/get_commentaires.php'); 
 include_once('modele/get_billet.php');
 include_once('modele/signal_commentaire.php');  
 include_once('modele/post_commentaire.php');
 
 function affichage_commentaires() {
-	nouveau_commentaire();
-	$idBillet=$_GET['billet'];
-	$billet = get_billet($idBillet);
-	$commentaires = get_commentaires(0, 10, $idBillet);
-	 
-	// On effectue du traitement sur les données (contrôleur) 
-	// Ici, on doit surtout sécuriser l'affichage 
-	// echo "<pre>";
-	// var_dump($commentaires);
-	foreach($commentaires as $cle => $commentaire) 
-	{ 
-		// var_dump($commentaire);
-	    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
-	    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
-	} 
-	// var_dump($commentaires);	
-	// On affiche la page (vue) 
-	include_once('vue/commentaires.php'); 
-
-}
-
-function nouveau_commentaire() {
 	if (!empty($_POST)) {
 		post_commentaire();
 	}
+	$idBillet=$_GET['billet'];
+	$billet = get_billet($idBillet);
+	$commentaires = get_commentaires(0, 10, $idBillet);
+	
+	// Ici, on doit surtout sécuriser l'affichage 
+	foreach($commentaires as $cle => $commentaire) 
+	{ 
+	    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
+	    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
+	} 
+	include_once('vue/commentaires.php'); 
 }
 
 function signalement_commentaire() {
 	if (!empty($_POST['idCommentaireSignaled'])) {
-		signal_commentaire();
+		$idCommentaire = $_POST['idCommentaireSignaled'];
+		signal_commentaire($idCommentaire);
 	}
 	$idBillet=$_POST['id2_billet'];
 	header('Location: blog.php?section=commentaires&billet='.$idBillet);
--- modele/signal_commentaire.php
+++ modele/signal_commentaire.php
@@ -1,13 +1,8 @@
 <?php
-
-// il faudra insérer l'id du commentaire voire l'id du Billet et faire global $bdd
-function signal_commentaire() {
+function signal_commentaire($id_commentaire) {
 	global $bdd;
-	if (isset($_POST['idCommentaireSignaled'])) {
-	 $id = $_POST['idCommentaireSignaled'];
-	 $sql = "UPDATE commentaires SET signalement = 1 WHERE id = :id"; 
-	 $q = array('id' => $id);
-	 $req = $bdd -> prepare($sql);
-	 $req -> execute($q);
-	}
+
+	$sql = "UPDATE commentaires SET signalement = 1 WHERE id = :id_commentaire"; 
+	$req = $bdd -> prepare($sql);
+	$req -> execute(array('id_commentaire' => $id);
 }
