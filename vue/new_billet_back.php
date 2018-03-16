ecc015b469b7fe2fe06b3a88b96d0f1fd1191ba4
anouchk <analutzky@gmail.com>
2018-03-16 16:17:40 +0100

    tinyMCE last

--- vue/new_billet_back.php
+++ vue/new_billet_back.php
@@ -6,13 +6,13 @@
         <title>Billet simple pour l'Alaska </title>
 
     	<link href="assets/css/style.css" rel="stylesheet" /> 
-    	<!-- <script type="text/javascript" src="XXXXX/tiny_mce/tiny_mce.js"></script>
-    	<script type="text/javascript">
-	        tinyMCE.init({
-	            mode : "textareas",
-	            theme : "simple"
-	        });
-     	</script> -->
+        <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+        <script >
+            tinymce.init({
+                selector : "#tinyMCEarea"
+            });
+        </script>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
@@ -34,7 +34,7 @@
 
 			<form action="index.php?section=enregistrer_nouveau_billet" method="post">
 				<p><label> Titre :</label> <input class="input_large" type="text" name="titre_billet" value="" ></p>
-				<p><label>Contenu de l'épisode :</label> <textarea class="input_large" type="text" name="contenu_billet" rows="20" cols="90"></textarea></p>
+				<p><label>Contenu de l'épisode :</label> <textarea id="tinyMCEarea" class="input_large" type="text" name="contenu_billet" rows="20" cols="90"></textarea></p>
 				<p><input type="submit" class="btn btn-primary" value="Enregistrer l'épisode" ></p>
 			</form>
 
