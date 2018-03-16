e180c02343083ce27a47a6d8178f94e869574a72
anouchk <analutzky@gmail.com>
2018-03-16 18:29:59 +0100

    suppresion de htmlspcialchars du fait de tinyMCE

--- controleur/billets.php
+++ controleur/billets.php
@@ -4,10 +4,10 @@ include_once('modele/get_billets.php');
 function billets_front_affichage_billets() {	
 	$billets = get_billets(0, 30);  
 	// Ici, on doit surtout sécuriser l'affichage 
-	foreach($billets as $cle => $billet) 
-	{ 
-	    $billets[$cle]['titre'] = htmlspecialchars($billet['titre']); 
-	    $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu'])); 
-	} 
+	// foreach($billets as $cle => $billet) 
+	// { 
+	//     $billets[$cle]['titre'] = htmlspecialchars($billet['titre']); 
+	//     $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu'])); 
+	// } 
 	include_once('vue/billets.php'); 
 }
