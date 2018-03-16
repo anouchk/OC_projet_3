bc18b5269f9265ef03b8c03f27b1ab5e1793a7a0
anouchk <analutzky@gmail.com>
2018-03-16 11:18:12 +0100

    mise en forme des boutons dans la vue front

--- vue/commentaires.php
+++ vue/commentaires.php
@@ -10,6 +10,8 @@
 		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+		<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/solid.js" integrity="sha384-+Ga2s7YBbhOD6nie0DzrZpJes+b2K1xkpKxTFFcx59QmVPaSA8c7pycsNaFwUK6l" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.8/js/fontawesome.js" integrity="sha384-7ox8Q2yzO/uWircfojVuCQOZl+ZZBg2D2J5nkpLqzH1HY0C1dHlTKIbpRz/LG23c" crossorigin="anonymous"></script>
 
     </head>
 
@@ -20,9 +22,9 @@
         } elseif (isset($_SESSION) && ($_SESSION['connected']=="non")) {
             echo "<h3><a href='blog.php?section=login'>Admin</a></h3>";
         } ?>
-        
+
         <h1>Billet simple pour l'Alaska</h1>
-        <a href="blog.php?section=index">Retour à la liste des billets</a>
+        <a href="blog.php?section=index" class="btn btn-secondary"><i class="fas fa-arrow-alt-circle-left"></i> Retour à la liste des billets</a>
 
 			<div class="news">		
 				<h3> 
@@ -41,7 +43,7 @@
 				<p><label> Pseudo</label> : <input type="text" name="pseudo"></p>
 				<p><label> Message</label> : <input type="text" name="message"></p>
 				<input type ="hidden" name="id2_billet" value="<?php echo $_GET['billet']?>">
-				<p><input type="submit" ></p>
+				<p><input class="btn btn-secondary" type="submit" ></p>
 			</form>	
 
 
@@ -65,7 +67,7 @@
 		      	<form method="post" action="blog.php?section=signalement_commentaire&billet=<?php echo $billet['id']; ?>">
        				<input type="hidden" name="idCommentaireSignaled" value="<?php echo $commentaire['id']; ?>"/>
        				<input type ="hidden" name="id2_billet" value="<?php echo $_GET['billet']?>">
-       				<p><input type="submit" value="Signaler ce commentaire"></p>
+       				<p><input type="submit" class="btn btn-secondary" value="Signaler ce commentaire"></p>
     			</form>
 			<?php	
 			} // fin de la boucle des commentaires 
