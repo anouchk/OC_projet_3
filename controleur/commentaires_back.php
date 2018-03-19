a8609484851e403e6816c796925683468bcd5081
anouchk <analutzky@gmail.com>
2018-03-19 01:52:11 +0100

    simplification controleur/commentaires_back

--- controleur/commentaires_back.php
+++ controleur/commentaires_back.php
@@ -5,36 +5,26 @@ include_once('modele/get_billet.php');
 
 function commentaires_back_affichage_commentaires()
 {
-    $idBillet = $_GET['billet'];
-    $commentaires = recuperation_commentaires_du_billet($idBillet)[0];
-    $billet = recuperation_commentaires_du_billet($idBillet)[1];
-    include_once('vue/commentaires_back.php');
+	if (!empty($_GET['billet'])) {
+		$idBillet = $_GET['billet'];
+	    $commentaires = get_commentaires(0, 30, $idBillet);
+		// on sécurise l'affichage des commentaires
+		foreach($commentaires as $cle => $commentaire) 
+		{ 
+		    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
+		    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
+		} 
+		// lancer la requête de récupération des données du billet pour pouvoir afficher le titre du billet en haut de la liste des commentaires
+		$billet = get_billet($idBillet);
+	    include_once('vue/commentaires_back.php');
+	}   
 }
 
 function commentaires_back_suppression_commentaire()
-{
-    $idBillet = $_POST['idBillet'];
-    $commentaires = recuperation_commentaires_du_billet($idBillet)[0];
-    header('Location: index.php?section=commentaires_back&billet='.$idBillet);
-}
-
-function recuperation_commentaires_du_billet($idBillet) {
-	$commentaires = get_commentaires(0, 30, $idBillet);
-	// on sécurise l'affichage des commentaires
-	foreach($commentaires as $cle => $commentaire) 
-	{ 
-	    $commentaire[$cle]['auteur'] = htmlspecialchars($commentaire['auteur']); 
-	    $commentaire[$cle]['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
-	} 
-
-	// lancer la requête de récupération des données du billet pour pouvoir afficher le titre du billet en haut de la liste des commentaires
-	$billet = get_billet($idBillet);
-	
-	// lancer la requête de suppression du commentaire
-	if (!empty($_POST['idCommentaire'])) {
-		delete_commentaire($_POST['idCommentaire']);
+{	if (!empty($_POST['idCommentaire'])) {
+		$idBillet = $_POST['idBillet'];
+		$idCommentaire = $_POST['idCommentaire'];
+		delete_commentaire($idCommentaire);
 		header('Location: index.php?section=commentaires_back&billet='.$idBillet);
-	}
-
-	return [$commentaires, $billet];
+	}    
 }
