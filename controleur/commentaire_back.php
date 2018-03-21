fc5622fd8f6c437909cf02444f4e2b712879cea7
anouchk <analutzky@gmail.com>
2018-03-21 19:17:13 +0100

    instanciation du modele dans CommentaireBackControleur

--- controleur/commentaire_back.php
+++ controleur/commentaire_back.php
@@ -1,29 +1,30 @@
 <?php
-include_once('modele/get_commentaire.php'); 
-include_once('modele/get_billet.php');  
-include_once('modele/signal_commentaire.php');  
-include_once('modele/modify_commentaire.php');
+include_once('modele/Service/BilletManager.php'); 
+include_once('modele/Service/CommentaireManager.php'); 
 
 class CommentaireBackControleur {
 
 	public function affichage_commentaire_a_modifier() {
 		if (!empty($_POST['idCommentaireModified'])) {
 			$idCommentaire = $_POST['idCommentaireModified'];
-			$commentaire = get_commentaire($idCommentaire);
+			$commentaireManager =  new CommentaireManager;
+			$commentaire = $commentaireManager->get_commentaire($idCommentaire);
 			// sécurisons l'affichage 
 			$commentaire['auteur'] = htmlspecialchars($commentaire['auteur']); 
 		    $commentaire['commentaire'] = nl2br(htmlspecialchars($commentaire['commentaire'])); 
 
 		    $idBillet = $_POST['id2_billet'];
-		    $billet = get_billet($idBillet);
+		    $billetManager = new BilletManager;	
+		    $billet = $billetManager->get_billet($idBillet);
 			include_once('vue/commentaire_back.php');
 		}
 	}
 
 	public function enregistrement_modification_commentaire() {
 		if (!empty($_POST['idCommentaireModified'])) {	
-			update_commentaire();
-			unsignal_commentaire($_POST['idCommentaireModified']);
+			$commentaireManager =  new CommentaireManager;
+			$commentaireManager->update_commentaire();
+			$commentaireManager->unsignal_commentaire($_POST['idCommentaireModified']);
 			$idBillet=$_POST['id2_billet'];
 			header('Location: index.php?section=commentaires_back&billet='.$idBillet);
 		}
