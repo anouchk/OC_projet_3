98479179effb02f9ab32ecb571b3725a5ece48b4
anouchk <analutzky@gmail.com>
2018-03-21 18:51:33 +0100

    instanciation du modele dans BilletsControleur

--- controleur/billets.php
+++ controleur/billets.php
@@ -1,10 +1,13 @@
 <?php 
-include_once('modele/get_billets.php'); 
+include_once('modele/Service/BilletManager.php'); 
 
 class BilletsControleur {
 
 	public function billets_front_affichage_billets() {	
-	$billets = get_billets(0, 30);  
+
+	$billetManager = new BilletManager;
+    $billets = $billetManager->get_billets(0, 30);
+
 	include_once('vue/billets.php'); 
 	}
 
