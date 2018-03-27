6a4f09fcdf9fd7bd90a01fbc00a471ff7bed3ee1
anouchk <analutzky@gmail.com>
2018-03-27 23:23:46 +0200

    BilletsControleur

--- controleur/BilletsControleur.php
+++ controleur/BilletsControleur.php
@@ -4,12 +4,21 @@ use modele\Service\BilletManager;
 
 class BilletsControleur extends Controller {
 
+	private $billetManager;
+
+	public function __construct($billetManager) {
+		$this->billetManager = $billetManager;
+	}
+
 	public function billets_front_affichage_billets() {	
 
-	$billetManager = new BilletManager();
     $billets = $billetManager->get_billets(0, 30);
 
-	include_once('vue/billets.php'); 
+    $tableau = [
+    	'billets' => $billets,
+    ];
+
+	$this->render('vue/billets.php', $tableau); 
 	}
 
 }
