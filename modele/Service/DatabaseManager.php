abc8fefe522552363957926ec0e4b9cc5e2cbdf7
anouchk <analutzky@gmail.com>
2018-03-28 17:37:44 +0200

    set PDO as dependency of DatabaseManager

--- modele/Service/DatabaseManager.php
+++ modele/Service/DatabaseManager.php
@@ -3,23 +3,16 @@ namespace modele\Service;
 
 abstract class DatabaseManager {
 
-	private static $bdd;
+	private $pdo;
 
 	// connexion à la base de données
-	public function __construct() {
-		// Connexion à la base de données
-		try {
-			if (self::$bdd === null) {
-				self::$bdd = new \PDO('mysql:host=localhost;dbname=OC_projet_3;charset=utf8', 'root', 'root', array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION));
-			}
-		} catch(\Exception $e) {
-				die('Erreur : '.$e->getMessage());
-		}
+	public function __construct(\PDO $pdo) {
+		$this->pdo = $pdo;
 	}
 
 	// getter
 	public function getBdd() {
-		return self::$bdd;
+		return $this->pdo;
 	}
 
 }
