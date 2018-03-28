840dd33db31a887d08f2af1d30d4786daa2cb376
anouchk <analutzky@gmail.com>
2018-03-28 17:16:25 +0200

    the Container can return a PDO instance

--- modele/Service/Container.php
+++ modele/Service/Container.php
@@ -3,6 +3,18 @@ namespace modele\Service;
 
 class Container {
 
+	private $pdo;
+ 
+	public function getPDO()
+	{
+		if ($this->pdo === null) {
+			$this->pdo = new \PDO('mysql:host=localhost;dbname=OC_projet_3;charset=utf8', 'root', 'root');
+			$this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		}
+
+		return $this->pdo;
+	}
+
 	private $billetManager;
 
 	public function getBilletManager() {
