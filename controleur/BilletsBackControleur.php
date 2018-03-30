0321d0f0d04ee6983656560c42d090c805366c53
anouchk <analutzky@gmail.com>
2018-03-30 11:01:00 +0200

    BilletManager est une propriété du Container, pas une méthode

--- controleur/BilletsBackControleur.php
+++ controleur/BilletsBackControleur.php
@@ -14,13 +14,13 @@ class BilletsBackControleur extends Controller {
 	}
 
 	public function billets_back_affichage_billets() {	
-		$billetManager = $this->billetManager();
+		$billetManager = $this->billetManager;
 		$billets = $billetManager->get_billets(0,30);
 
 		// Ici, on doit surtout sécuriser l'affichage. Doit-on vraiment ?
 		foreach($billets as $cle => $billet) 
 		{ 
-			$commentaireManager =  new CommentaireManager();
+			$commentaireManager =  $this->commentaireManager;
 			$commentaires = $commentaireManager->get_commentaires(0,30, $billet['id']);
 		    $billets[$cle]['titre'] = htmlspecialchars($billet['titre']); 
 		    $billets[$cle]['contenu'] = nl2br(htmlspecialchars($billet['contenu'])); 
