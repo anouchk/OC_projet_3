f515d26617dc6928e492e7d74997e0081634baff
anouchk <analutzky@gmail.com>
2018-04-08 19:04:47 +0200

    terminer de mettre en place l'interface Container dans le routeur

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ if(!isset($_SESSION['connected'])) {
     $_SESSION['connected'] = "non";
 }
 
-
-
 // Autoloader
 require_once "modele/Service/Autoloader.php";
 \modele\Service\Autoloader::register();
@@ -16,9 +14,8 @@ require __DIR__.'/config/configuration.php';
 $container = new \modele\Service\Container($configuration);
 
 $billetManager = $container->get('billetManager');
-var_dump($billetManager);
-
-var_dump($container->has('billetManager'));
+// var_dump($billetManager);
+// var_dump($container->has('billetManager'));
 
 // Routeur
 if (!isset($_GET['section']) OR $_GET['section'] == 'index') {
@@ -44,46 +41,46 @@ if (!isset($_GET['section']) OR $_GET['section'] == 'index') {
     $loginControleur->logout();    
 } else if($_GET['section'] == 'billets_back') {
     #include_once('controleur/billets_back.php');
-    $billetsBackControleur = new \controleur\BilletsBackControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $billetsBackControleur = new \controleur\BilletsBackControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $billetsBackControleur->billets_back_affichage_billets();  
 } else if ($_GET['section'] == 'commentaires_back') {
     #include_once('controleur/commentaires_back.php');
-    $commentairesBackControleur = new \controleur\CommentairesBackControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $commentairesBackControleur = new \controleur\CommentairesBackControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $commentairesBackControleur->commentaires_back_affichage_commentaires();
 } else if ($_GET['section'] == 'suppression_commentaire') {
     #include_once('controleur/commentaires_back.php');
-    $commentairesBackControleur = new \controleur\CommentairesBackControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $commentairesBackControleur = new \controleur\CommentairesBackControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $commentairesBackControleur->commentaires_back_suppression_commentaire();
 } else if ($_GET['section'] == 'signalement_commentaire') {
     #include_once('controleur/commentaires.php');
-    $commentairesControleur = new \controleur\CommentairesControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $commentairesControleur = new \controleur\CommentairesControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $commentairesControleur->commentaires_front_signalement_commentaire();
 } else if ($_GET['section'] == 'modification_commentaire') {
     #include_once('controleur/commentaire_back.php');
-    $commentaireBackControleur = new \controleur\CommentaireBackControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $commentaireBackControleur = new \controleur\CommentaireBackControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $commentaireBackControleur->affichage_commentaire_a_modifier();
 } else if ($_GET['section'] == 'enregistrer_modification_commentaire') {
     #include_once('controleur/commentaire_back.php');
-    $commentaireBackControleur = new \controleur\CommentaireBackControleur($container->getBilletManager(), $container->getCommentaireManager());
+    $commentaireBackControleur = new \controleur\CommentaireBackControleur($container->get('billetManager'), $container->get('commentaireManager'));
     $commentaireBackControleur->enregistrement_modification_commentaire();
 } else if ($_GET['section'] == 'modification_billet') {
     #include_once('controleur/billet_back.php');
-    $billetBackControleur = new \controleur\BilletBackControleur($container->getBilletManager());
+    $billetBackControleur = new \controleur\BilletBackControleur($container->get('billetManager'));
     $billetBackControleur->affichage_billet_a_modifier() ;
 } else if ($_GET['section'] == 'enregistrer_modification_billet') {
     #include_once('controleur/billet_back.php');
-    $billetBackControleur = new \controleur\BilletBackControleur($container->getBilletManager());
+    $billetBackControleur = new \controleur\BilletBackControleur($container->get('billetManager'));
     $billetBackControleur->enregistrement_modification_billet();
 } else if ($_GET['section'] == 'creation_billet') {
     #include_once('controleur/billet_back.php');
-    $billetBackControleur = new \controleur\BilletBackControleur($container->getBilletManager());
+    $billetBackControleur = new \controleur\BilletBackControleur($container->get('billetManager'));
     $billetBackControleur->affichage_billet_a_creer() ;
 } else if ($_GET['section'] == 'enregistrer_nouveau_billet') {
     #include_once('controleur/billet_back.php');
-    $billetBackControleur = new \controleur\BilletBackControleur($container->getBilletManager());
+    $billetBackControleur = new \controleur\BilletBackControleur($container->get('billetManager'));
     $billetBackControleur->enregistrement_nouveau_billet();
 } else if ($_GET['section'] == 'suppression_billet') {
     #include_once('controleur/billet_back.php');
-    $billetBackControleur = new \controleur\BilletBackControleur($container->getBilletManager());
+    $billetBackControleur = new \controleur\BilletBackControleur($container->get('billetManager'));
     $billetBackControleur->suppression_billet();
 }
