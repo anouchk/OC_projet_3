a30f44051a4c77ffa6bf258f63fd0e6c1aa1febd
anouchk <analutzky@gmail.com>
2018-04-12 08:20:09 +0200

    extrait la section de la route, on découpe l'url et récupère ce qu'il y a après section=

--- modele/Service/Router.php
+++ modele/Service/Router.php
@@ -14,12 +14,17 @@ class Router {
 			'controller' => 'CommentairesControleur',
 			'action' => 'commentaires_front_affichage_commentaires',
 		],
+		// 'login_traitement_formulaire' => [
+		// 	'controller' => 'LoginControleur',
+		// 	'action' => 'login_traitement_formulaire($bdd)',
+		// ],
+		
 	];
 
 	//ici request_uri ça va être toutes les string de type "index.php?section=billets_back" etc.
 	public function __construct($request_uri) {
 
-		$this->request_uri = $request_uri;
+		$this->request_uri = substr($request_uri, strpos($request_uri, "=") + 1);
 	}
 
 	/**
