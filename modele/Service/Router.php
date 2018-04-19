4e99c00d28883ec211c591d923a041b30cc1cd9c
anouchk <analutzky@gmail.com>
2018-04-19 17:04:48 +0200

    ajouts

--- modele/Service/Router.php
+++ modele/Service/Router.php
@@ -84,7 +84,7 @@ class Router {
 	public function __construct($request_uri) {
 
 		$this->request_uri = substr($request_uri, strpos($request_uri, "=") + 1);
-		// var_dump($this->request_uri);
+		var_dump($this->request_uri);
 	}
 
 	/**
