c694ef78bd254bd428024c216fa0ee2eabc299f6
anouchk <analutzky@gmail.com>
2018-05-01 23:18:34 +0200

    ajout d'une valeur par défaut à signalement

--- modele/Service/CommentaireManager.php
+++ modele/Service/CommentaireManager.php
@@ -7,14 +7,16 @@ class CommentaireManager extends DatabaseManager {
 	public function add_commentaire () {
 
 	    $bdd = $this->getBdd();
+	    $signalement = 0;
 
 		// Effectuer ici la requête qui insère le message reçu avec $_POST dans la base de données 
-		$requete = $bdd->prepare('INSERT INTO commentaires(id_billet,auteur,commentaire, date_commentaire) VALUES(:id_billet, :auteur, :commentaire, :date_commentaire)'); 
+		$requete = $bdd->prepare('INSERT INTO commentaires(id_billet,auteur,commentaire, date_commentaire, signalement) VALUES(:id_billet, :auteur, :commentaire, :date_commentaire, :signalement)'); 
 		$date = date('Y-m-d H:i:s');
 		$requete->bindParam(':id_billet', $_POST['id2_billet']);
 		$requete->bindParam(':auteur', $_POST['pseudo']);
 		$requete->bindParam(':commentaire', $_POST['message']);
 		$requete->bindParam(':date_commentaire', $date);
+		$requete->bindParam(':signalement', $signalement);
 		$requete->execute();
 	}
 
