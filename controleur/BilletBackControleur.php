177b6da834055a6e63ccee58e04f66f213985e8e
anouchk <analutzky@gmail.com>
2018-05-03 09:08:47 +0200

    modification des include pour les adapter à Twig dans BilletBackControleur.php

--- controleur/BilletBackControleur.php
+++ controleur/BilletBackControleur.php
@@ -18,7 +18,7 @@ class BilletBackControleur extends Controller {
 				$view_params = [
 					'billet' => $billet,
 				];
-				$this->render('vue/billet_back.php', $view_params); 
+				$this->render('billet_back.php', $view_params); 
 			}
 		} else {
 			$this->redirect('index.php?section=login');
@@ -36,7 +36,7 @@ class BilletBackControleur extends Controller {
 	public function affichage_billet_a_creer() {
 		if (isset($_SESSION) && ($_SESSION['connected']=="oui")) {
 			$view_params = [];
-			$this->render('vue/new_billet_back.php', $view_params); 
+			$this->render('new_billet_back.php', $view_params); 
 		} else {
 			$this->redirect('index.php?section=login');
 		}
@@ -46,13 +46,13 @@ class BilletBackControleur extends Controller {
 		if (!empty($_POST['titre_billet'])&& !empty($_POST['contenu_billet'])) {
 			$billetManager = $this->billetManager;	
 			$billetManager->add_billet();
-			header('Location: index.php?section=billets_back');
+			$this->redirect('index.php?section=lection=billets_back');
 		} else {
 			$message = "Le contenu et/ou le titre sont vides. Veuillez remplir le formulaire.";
 			$view_params = [
 				$billet = $billetManager->get_billet($idBillet)
 			];
-			$this->render('vue/new_billet_back.php', $view_params); 
+			$this->render('new_billet_back.php', $view_params); 
 		}
 	}
 
