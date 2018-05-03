7592799e305bb36a282b90e3cb2630924a99848a
anouchk <analutzky@gmail.com>
2018-05-03 09:37:48 +0200

    twig dans les vues de billet_bacl, billets et commentaires

--- vue/billets.php
+++ vue/billets.php
@@ -13,14 +13,12 @@
 
     <body>
         
-        {#
-        <?php if ($connected=="oui") {
+        {%  if connected=="oui"  %}
             echo "<h3><a href='index.php?section=logout'>Deconnexion</a></h3>";
             echo "<h3><a href='index.php?section=login'>Admin</a></h3>";
-        } elseif ($connected=="non") {
+        {%  elseif connected=="non"  %}
             echo "<h3><a href='index.php?section=login'>Admin</a></h3>";
-        } ?>
-        #}
+        {% endif %}
         
         <h1>Billet simple pour l'Alaska</h1>
         <p> Découvrez le nouveau roman de l'acteur et écrivain Jean Forteroche, à mesure qu'il se construit.</p>
--- vue/commentaires.php
+++ vue/commentaires.php
@@ -16,66 +16,60 @@
     </head>
 
     <body>
-    	<?php if ($connected=="oui")  {
+    	{%  if connected=="oui"  %}
             echo "<h3><a href='index.php?section=logout'>Deconnexion</a></h3>";
             echo "<h3><a href='index.php?section=login'>Admin</a></h3>";
-        } elseif ($connected=="non")  {
+        {%  elseif connected=="non"  %}
             echo "<h3><a href='index.php?section=login'>Admin</a></h3>";
-        } ?>
+       {% endif %}
 
         <h1>Billet simple pour l'Alaska</h1>
         <a href="index.php?section=index" class="btn btn-secondary"><i class="fas fa-arrow-alt-circle-left"></i> Retour à la liste des billets</a>
 
 			<div class="news">		
 				<h3> 
-					<?php echo $billet->getTitre() ; ?>
-					<em><?php echo $billet->getDateCreation() ; ?></em>
+					{{ billet.getTitre }}
+					<em>le {{ billet.getDateCreation }}</em>
 				</h3>
 
 				<p> 
-					<?php echo $billet->getContenu() ; ?> 
+					{{ billet.getContenu }}
 				</p>
 			 </div>
 
 			<h2> Commentaires </h2>
 
-			<form action="index.php?section=commentaires&billet=<?php echo $billet->getId(); ?>" method="post">
+			<form action="index.php?section=commentaires&billet={{ billet.getId }}" method="post">
 				<p><label> Pseudo</label> : <input type="text" name="pseudo"></p>
 				<p><label> Message</label> : <input type="text" name="message"></p>
-				<input type ="hidden" name="id2_billet" value="<?php echo $idBillet ?>">
+				<input type ="hidden" name="id2_billet" value="{{ idBillet }}">
 				<p><input class="btn btn-secondary" type="submit" ></p>
 			</form>	
 
 
-		     <?php
-				foreach ($commentaires as $commentaire)	{
-			?>
+		    {% for commentaire in commentaires %} 
 				
-					<strong><?php echo $commentaire->getAuteur() ; ?></strong>
-					Le <?php echo $commentaire->getDateCommentaire() ; ?>
+					<strong>{{ commentaire.getAuteur }}</strong>
+					Le {{ commentaire.getDateCommentaire }}
 				
-				<div><?php echo $commentaire->getCommentaire() ; ?></div>
+				<div>{{ commentaire.getCommentaire }}</div>
 				
 
-					<?php // bouton de signalement
-						if ($commentaire->getSignalement() == 1) {
-							$signaled='<button id="<?php echo $commentaire->getId() ; ?>"
-							 class="btn btn-danger">Commentaire signalé</button><br><br>';
-							echo $signaled ;
-						} else {
-					?>
+					{%  if {{ commentaire.getSignalement }} == 1 %}
+						signaled='<button id=" {{ commentaire.getId }}"
+						class="btn btn-danger">Commentaire signalé</button><br><br>'
+						echo signaled 
+					{%  else  %}
+
 		
-							<!-- Pour signaler : je veux modifier le boléen sur le commentaire dont l'id sera récupéré en POST-->
-					      	<form method="post" action="index.php?section=signalement_commentaire&billet=<?php echo $billet->getId(); ?>">
-			       				<input type="hidden" name="idCommentaireSignaled" value="<?php echo $commentaire->getId(); ?>"/>
-			       				<input type ="hidden" name="id2_billet" value="<?php echo $billet->getId(); ?>">
+
+					      	<form method="post" action="index.php?section=signalement_commentaire&billet={{ billet.getId }} ">
+			       				<input type="hidden" name="idCommentaireSignaled" value="{{ commentaire.getId }}"/>
+			       				<input type ="hidden" name="id2_billet" value="{{ billet.getId }}">
 			       				<p><input type="submit" class="btn btn-secondary" value="Signaler ce commentaire">
 			    			</form>
 			    			
-					<?php	
-						}		
-			} // fin de la boucle des commentaires 
-			?>
+				{% endfor %}
 			
 			
     </body>
