56c7e38107c5bea14638e5e8331f7d417c23099e
anouchk <analutzky@gmail.com>
2018-05-22 21:08:32 +0200

    utilisation de l'entité Auteur dans le modèle, c'est-à-dire dans LoginManager

--- controleur/LoginControleur.php
+++ controleur/LoginControleur.php
@@ -1,5 +1,6 @@
 <?php
 namespace controleur;
+use modele\Entity\Auteur;
 
 class LoginControleur extends Controller {
 
@@ -27,8 +28,13 @@ class LoginControleur extends Controller {
     {
         // Etape 1 : recupérer dans la bdd la ligne qui correspond au pseudo
         $loginManager = $this->logintManager;
-        $resultat = $loginManager->Recuperation_ligne_correspondant_au_pseudo();
-        // etape 2 : comparer le pass du hash avec celui entré par le formulaire de connexion
+        // On instancie l'entité Auteur
+        $auteur = new Auteur();
+        // Le contrôleur lance le setter récupérant le pseudo entré par l'utilisateur à la connexion
+        $auteur->setPseudo($_POST['pseudo']);
+        // On demande au modèle d'effectuer la requête récupérant le mot de passe crypté correspondant au pseudo entré par l'utilisateur
+        $resultat = $loginManager->Recuperation_ligne_correspondant_au_pseudo($auteur);
+        // etape 2 : comparer le mot de passe crypté de la base de données avec celui entré par l'utilisateur dans le formulaire de connexion
         if (password_verify($_POST['pass'], $resultat['pass'])) {
             $_SESSION['connected'] = "oui";
             // si les identifiants sont les bons, on renvoie vers billets_back.php
--- modele/Service/LoginManager.php
+++ modele/Service/LoginManager.php
@@ -1,16 +1,18 @@
 <?php
 namespace modele\Service;
+use modele\Entity\Auteur;
 
 class LoginManager extends DatabaseManager {
 
-	public function Recuperation_ligne_correspondant_au_pseudo() {
+	/**
+	 * Retourne le mot de passe crypté correspondant au pseudo entré par l'utilisateur
+	 */
+	public function Recuperation_ligne_correspondant_au_pseudo(Auteur $auteur) {
 		$bdd = $this->getBdd();
 		// Etape 1 : recupérer dans la bdd la ligne qui correspond au pseudo
 		// Préparer la requête (:pseudo ça correspondra à $pseudo)
-		$req = $bdd->prepare('SELECT id, pass, pseudo FROM auteur WHERE pseudo = :pseudo');
-		// Initialisation de la variable récupérant le pseudo entré par l'utilisateur à la connexion
-		$pseudo = $_POST['pseudo'];
-
+		$req = $bdd->prepare('SELECT id, pass, pseudo FROM auteur WHERE pseudo = :pseudo');		
+		$pseudo = $auteur->getPseudo();
 		// Exécuter la requête : ça exécute et ça nous retourne le nombre de lignes affectées
 		$req->execute(array('pseudo' => $pseudo));
 		// Fetch permet de récupérer le résultat de la requête et de le renvoyer sous une certaine forme (tableau par exemple)
